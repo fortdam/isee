c9233fb4bd860fa1b2dfc882c8d985a0d58d95d9
sdduser <sdduser@aclgcl-ubnt.(none)>
2015-12-29 22:14:56 +0800

    pass params into the genreport

--- isee_server/tools/generate_report.js
+++ isee_server/tools/generate_report.js
@@ -6,43 +6,64 @@ var project = 'idol4_151207';
 var product = '';
 var user = '';
 
-isee_db.open(function(){
-	isee_db.getCommentSummary(project, product, user, function(data){
-		var currScene = "";
-		var currIndex = "";
 
-		var os = fs.createWriteStream('output.txt');
-		os.write("The Summary of "+project+ ":\r\n\r\n");
+if (process.argv[2].match(/help/i)){
+	console.log("The usage of the genReport:");
+	console.log(" node generate_report.js <project_folder> [product_name [user]]");
+}
+else{
+	genReport(process.argv[2], process.argv[3], process.argv[4]);
+}
 
-		data.forEach(function(v,i,a){
-			if (currScene != v.scene){
-				currIndex = '';
-				os.write("====="+v.scene+"=====\r\n");
-				currScene = v.scene;
-			}
-			if (currIndex != v.index){
-				os.write('  #'+v.index+"\r\n");
-				currIndex = v.index;
-			}
-			var strComment = "    "+v.user;
+function genReport(project, product, user){
+	isee_db.open(function(){
+		if(product===null || product===undefined){
+			product = '';
+		}
 
-			while (strComment.length < 20){
-				strComment += ' ';
-			}
-			strComment += ("@"+v.product);
-			while (strComment.length < 34){
-				strComment += ' ';
-			}
-			strComment += ("#"+v.grade);
-			while (strComment.length < 42){
-				strComment += ' ';
-			}
-			strComment += (':'+v.review+'\r\n');
+		if(user===null || user===undefined){
+			user = '';
+		}
 
-			os.write(strComment);
-		});
+		isee_db.getCommentSummary(project, product, user, function(data){
+			var currScene = "";
+			var currIndex = "";
 
-		os.end();
-		isee_db.close();
+			var os = fs.createWriteStream('output.txt');
+			os.write("The Summary of "+project+ ":\r\n\r\n");
+
+			data.forEach(function(v,i,a){
+				if (currScene != v.scene){
+					currIndex = '';
+					os.write("====="+v.scene+"=====\r\n");
+					currScene = v.scene;
+				}
+				if (currIndex != v.index){
+					os.write('  #'+v.index+"\r\n");
+					currIndex = v.index;
+				}
+				var strComment = "    "+v.user;
+
+				while (strComment.length < 20){
+					strComment += ' ';
+				}
+				strComment += ("@"+v.product);
+				while (strComment.length < 34){
+					strComment += ' ';
+				}
+				strComment += ("#"+v.grade);
+				while (strComment.length < 42){
+					strComment += ' ';
+				}
+				strComment += (':'+v.review+'\r\n');
+
+				os.write(strComment);
+			});
+
+			os.end();
+			isee_db.close();
+		})
 	})
-})
+}
+
+
